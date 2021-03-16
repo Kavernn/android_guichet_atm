84ea878afb8e472f5ecc64ea210274ab1a5d9641
Vincent Pinard <kavernn@gmail.com>
2021-03-16 16:44:11 -0400

    Commit du java de l'admin

---
+++ app/src/main/java/com/julien/guichet_automatique_atm/AdminActivity.java
@@ -0,0 +1,27 @@
+package com.julien.guichet_automatique_atm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_activity);
+    }
+
+    public void onClickBtnPaiementInterets(View view) {
+    }
+
+    public void onClickBtnComptesCheques(View view) {
+    }
+
+    public void onClickBtnComptesEpargnes(View view) {
+    }
+
+    public void onClickListeClients(View view) {
+    }
+}
