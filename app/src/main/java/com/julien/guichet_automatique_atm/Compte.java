9caca176d09564cf41facf789f868600de2292c4
Your Name <you@example.com>
2021-03-23 10:40:36 -0400

    ajout du code classe compte

--- app/src/main/java/com/julien/guichet_automatique_atm/Compte.java
+++ app/src/main/java/com/julien/guichet_automatique_atm/Compte.java
@@ -1,25 +1,75 @@
 package com.julien.guichet_automatique_atm;
 
-public class Compte {
+/**
+ *
+ * @author user
+ */
+abstract public class Compte {
+
+    private int numeroNIP;
+    private String username;
+    private int numeroCompte;
+    private double soldeCompte;
+    private static int autoNumeroCompte = 10000;
+
+    public Compte (int nip, String username, double soldeCpt){
 
 
-    private int nip, numCompte;
-   private  double soldeCompte;
-  private Client le_client;
+        this.numeroNIP = nip;
+        this.username = username;
+        this.numeroCompte = ++ autoNumeroCompte;
+        this.soldeCompte = soldeCpt;
 
-    public Compte(int numCompte, Client le_client) {
-        this.numCompte = numCompte;
-        this.le_client = le_client;
-        this.soldeCompte =0;
     }
 
-    public void retirerMontant(int montant){
+
+    //
+
+    public int getNumeroNIP() {
+        return numeroNIP;
+    }
+
+    public void setNumeroNIP(int numeroNIP) {
+        this.numeroNIP = numeroNIP;
+    }
+
+    public int getNumeroCompte() {
+        return numeroCompte;
+    }
+
+
+    public double getSoldeCompte() {
+        return this.soldeCompte;
+    }
+
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setSoldeCompte(double soldeCompte) {
+        this.soldeCompte = soldeCompte;
+    }
+
+    public abstract void retrait(double amount);
+
+    public abstract void depot(double amount);
+
+    public static int getAutoNumeroCompte() {
+        return autoNumeroCompte;
+    }
+
+    @Override
+    public String toString(){
+
+        String affichage = "Compte no: " + this.numeroCompte + " ******************** \n";
+        affichage += " Username: " + this.username;
+        return affichage;
 
     }
 
-    public void deposerMontant(int montant){
 
-    }
+
 
 
 }
