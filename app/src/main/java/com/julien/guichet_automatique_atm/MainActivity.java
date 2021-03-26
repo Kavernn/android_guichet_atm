73eec44de71f842383087a6bf77a2edc27324228
Your Name <you@example.com>
2021-03-26 15:05:24 -0400

    ajout du 26 mars sur MainActivity

--- app/src/main/java/com/julien/guichet_automatique_atm/MainActivity.java
+++ app/src/main/java/com/julien/guichet_automatique_atm/MainActivity.java
@@ -15,16 +15,20 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String extra_nip = "extra_nip";
     public static final String extra_utilisateur = "extra_utilisateur";
+    Guichet guichet;
+    static int increment =  0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        guichet.addClient("Sijlamassi", "Hicham", "hichamsijlamassi", 12345);
+        guichet.addClient("Vogler", "Julien", "julienvogler", 12345);
+        guichet.addClient("Pinard", "Vincent", "pinardvincent", 12345);
+        guichet.addAdmin("Hicham", "admin");
     }
 
-
-
-    static int increment =  0;
 
     public void onClickOK(View view) {
         // L'utilisateur n'a le droit qu'à trois essais :
@@ -45,10 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 //=============================================================================================VERSION AVEC ADMIN UNIQUE
-        /*//Si le nom d'utilisateur et le mot de passe sont "admin", alors on va sur l'activité conversion.class à l'aide d'une intention
+        //Si le nom d'utilisateur et le mot de passe sont "admin", alors on va sur l'activité conversion.class à l'aide d'une intention
         // Sinon on demande à l'utilisateur de rentrer les bonnes informations.
 
-        if ((le_mdp.equals("admin")) && (le_nom.equals("admin"))) {
+       /* if ((le_mdp.equals("admin")) && (utilisateur.equals("admin"))) {
             Intent intent = new Intent(this, GuichetActivity.class);
             startActivity(intent);
         }
@@ -57,30 +61,19 @@ public class MainActivity extends AppCompatActivity {
         }*/
 
 //=============================================================================================
-        //Connection d'un client de Guichet 1 -
+        // Connection d'un utilisateur :
 
-
-        Guichet guichet1;
-
-            if (guichet1.validerUtilisateur(nip,utilisateur )){
-           // Pas besoin. le_client = Guichet1.trouverClient(nip,utilisateur  );
-
+        if (guichet.validerUtilisateur(nip,utilisateur )){
             // Redirection vers l'activité Guichet
             Intent intent_guichet = new Intent(this, GuichetActivity.class);
             startActivity(intent_guichet);
-            // Faire passer nip et utilisateur dans l'intention
+            // Faire passer nip et utilisateur dans GuichetActivity
             intent_guichet.putExtra(extra_nip, nip);
             intent_guichet.putExtra(extra_utilisateur, utilisateur);
             }
-            else {
-                Toast.makeText(this, "Utilisateur ou mot de passe incorrect ", 2).show();
-                }
 
-        // Connection d'un administrateur de Guichet 1 :
-
-        if (guichet1.validerAdmin(nip,utilisateur )) {
-           // Pas besoin. le_client = Guichet1.trouverClient(nip,utilisateur );
-
+        // Connection d'un administrateur :
+        else if (guichet.validerAdmin(nip,utilisateur )) {
             //Redirection vers l'activité Admin
             Intent intent_admin = new Intent(this, AdminActivity.class);
             startActivity(intent_admin);
@@ -88,15 +81,12 @@ public class MainActivity extends AppCompatActivity {
             intent_admin.putExtra(extra_nip, nip);
             intent_admin.putExtra(extra_utilisateur, utilisateur);
         }
-            else {
+        else {
             Toast.makeText(this, "Utilisateur ou mot de passe incorrect ", 2).show();
         }
 
-
-//=============================================================================================
-                //On incrémente le compteur statique
+        //Incrémentation du compteur
         increment = increment +1;
-
 
     }
 
